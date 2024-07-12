0ed834ea3712942698319abe59eb335a9bca8a49
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-12 19:38:51 -0400

    Some ruby learning

--- ruby-programming/hello_world.rb
+++ ruby-programming/hello_world.rb
@@ -1,11 +1,1 @@
-# puts
 puts "Hello World!"
-
-# print
-print "Hi\n"
-print "I print in the same line"
-
-# p method
-p "\nI print in the same line but everything"
-p 5
-p "5"
--- ruby-programming/main.rb
+++ ruby-programming/main.rb
@@ -1,3 +1,4 @@
+# Reverse a array
 address = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 p address
 new_address = address.reverse!
